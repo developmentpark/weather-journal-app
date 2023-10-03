02d39b19525cdb9064a6f8d2d8ba47d772409516
J. N. Yupanqui P <jnyupanquip@gmail.com>
2023-10-03 07:10:31 -0500

    define and config static content directory for server

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.static("website"));
+
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
 app.listen(PORT, () => {

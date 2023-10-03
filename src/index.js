981d889ac1f8b656c816a65e037053c6eb4515cd
J. N. Yupanqui P <jnyupanquip@gmail.com>
2023-10-03 07:25:32 -0500

    add Morgan for request logging

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const api = require("./api");
+const morgan = require("morgan");
 
 const app = express();
 
 app.use(express.static("website"));
 
 app.use(express.json());
+app.use(morgan("combined"));
 app.use("/api", api);
 
 const PORT = process.env.PORT;

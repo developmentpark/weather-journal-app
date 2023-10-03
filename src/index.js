7a9ca527d344d8adc3ef3d4d53ae679c8cfc0fc7
J. N. Yupanqui P <jnyupanquip@gmail.com>
2023-10-03 07:23:11 -0500

    config API route and enable JSON data handling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const api = require("./api");
 
 const app = express();
 
 app.use(express.static("website"));
+
+app.use(express.json());
+app.use("/api", api);
 
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;

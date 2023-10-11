566fbf5e305007d354a6a741784140c3369e22f1
J. N. Yupanqui P <jnyupanquip@gmail.com>
2023-10-11 13:29:43 -0500

    added CORS support to the server #2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const api = require("./api");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.static("dist"));
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan("combined"));
 app.use("/api", api);

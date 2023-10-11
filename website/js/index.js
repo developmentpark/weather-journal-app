038d4479b4a3a1df3370a854549ee9d62ebca6a5
J. N. Yupanqui P <jnyupanquip@gmail.com>
2023-10-11 12:43:26 -0500

    implement climate server request using HTTP service mock #2
    The code has been modified to make weather requests to the server
    using a simulated HTTP service, this allows the presentation to
    progress while waiting for the real endpoints to be implemented on the server.

--- website/js/index.js
+++ website/js/index.js
@@ -1,10 +1,32 @@
 import geolocationService from "./geolocationService.js";
+import httpService from "./httpServiceFake.js";
+
+function objectToQuery(object) {
+  return Object.entries(object).reduce((acum, curr, idx, arr) => {
+    acum += `${curr[0]}=${curr[1]}`;
+    if (idx < arr.length - 1) {
+      acum += "&";
+    }
+    return acum;
+  }, "?");
+}
+
+const API = "http://localhost:34343/api";
+
+function getWeather(coords) {
+  const path = "/weather";
+  const query = objectToQuery(coords);
+  const url = API + path + query;
+  console.log(url);
+  return httpService.get(API + path + query);
+}
 
 function init() {
   geolocationService
     .getCoords()
-    .then((coords) => console.log(coords))
-    .catch((error) => console.warn(error.message));
+    .then((coords) => getWeather(coords))
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error.message));
 }
 
 document.addEventListener("DOMContentLoaded", init);

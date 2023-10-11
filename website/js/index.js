86d1c0028fbaa69958bd7bd13f58ca3200f8b6cb
J. N. Yupanqui P <jnyupanquip@gmail.com>
2023-10-11 08:18:02 -0500

    implemented geolocation with Web API #2

--- website/js/index.js
+++ website/js/index.js
@@ -1,1 +1,10 @@
-alert("Hello World");
+import geolocationService from "./geolocationService.js";
+
+function init() {
+  geolocationService
+    .getCoords()
+    .then((coords) => console.log(coords))
+    .catch((error) => console.warn(error.message));
+}
+
+document.addEventListener("DOMContentLoaded", init);

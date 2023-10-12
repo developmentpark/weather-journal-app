f4733393ba0090bf74c29b675fa6c6517bd4fecf
J. N. Yupanqui P <jnyupanquip@gmail.com>
2023-10-12 10:12:09 -0500

    added the endpoint for entry registration, implemented using a JSON
    To efficiently manage and store the data, we've implemented a JSON data
    structure.

--- src/utils/httpService.js
+++ src/utils/httpService.js
@@ -6,4 +6,19 @@ export default {
     }
     return await res.json();
   },
+
+  post: async function (url, data) {
+    const res = await fetch(url, {
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    if (!res.ok) {
+      throw new Error({ status: res.status, text: res.text });
+    }
+    return await res.json();
+  },
 };

9dce1b057f114b96cd6c5d7e35dee7948e3380fc
J. N. Yupanqui P <jnyupanquip@gmail.com>
2023-10-25 18:10:55 -0500

    refactor #11

--- src/website/js/index.js
+++ src/website/js/index.js
@@ -7,9 +7,9 @@ import { getEl } from "../utils/dom.js";
 
 const API = process.env.API;
 
-function getWeather(coords) {
+function getWeather(queryParams) {
   const path = "/weather";
-  const query = objectToQuery(coords);
+  const query = objectToQuery(queryParams);
   return httpService.get(API + path + query);
 }
 
@@ -64,8 +64,11 @@ document.addEventListener("DOMContentLoaded", () => {
   getEl("box-feeling__publish-btn").addEventListener("click", publish);
   getEl("search-box__button").addEventListener("click", () => {
     const zip = getEl("search-box__input").value;
-    getWeather({ zip })
-      .then((data) => renderWeatherView(data))
-      .catch((error) => console.log(error.message));
+    const zipFormat = /^[0-9]{5}$/;
+    if (zipFormat.test(zip.trim())) {
+      getWeather({ zip })
+        .then((data) => renderWeatherView(data))
+        .catch((error) => console.log(error.message));
+    }
   });
 });
